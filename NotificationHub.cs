b7a5e349dd23b5283992a10223716b768399905c
Youssef Samir <Youssef Samir@TDF41>
2025-02-18 10:20:52 +0200

    Add project files.

---
+++ NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+
+
+namespace NotificationHubLibrary
+{
+    public class NotificationHub : Hub
+    {
+        // This method pushes a notification message to all connected clients.
+        public void SendNotification(string message)
+        {
+            Clients.All.receiveNotification(message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NotificationHubLibrary.Startup))]
+namespace NotificationHubLibrary
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable CORS if needed.
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            // Map SignalR hubs to the default "/signalr" route.
+            app.MapSignalR();
+        }
+    }
+}
